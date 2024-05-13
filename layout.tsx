e71d48075ca9a1ca7817f732383e3d26ee4b61f6
TnC-page <salemelmabrouk07@gmail.com>
2024-05-13 10:06:39 +0200

    deploy task

---
+++ layout.tsx
@@ -0,0 +1,32 @@
+import AuthProvider from "@/components/Provider";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Mulish } from "next/font/google";
+import NavBar from "@/components/NavBar";
+
+
+
+export const metadata: Metadata = {
+  
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        suppressContentEditableWarning
+        suppressHydrationWarning
+       
+      >
+        <AuthProvider>
+          <NavBar/>
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
